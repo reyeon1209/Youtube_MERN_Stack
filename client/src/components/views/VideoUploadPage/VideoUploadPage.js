c4692fc38918de064c52d246d5a913ab0f7882ec
reyeon1209 <reyeon5368@naver.com>
2020-08-24 00:22:49 +0900

    비디오 업로드 FORM 만들기

---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,107 @@
+import React, { useState } from 'react'
+import { Form, Icon, Input, Button, message, Typography } from 'antd';
+import Dropzone from 'react-dropzone';
+
+const { TextArea } = Input;
+const { Title } = Typography;
+
+const PrivateOptions = [
+    { value: 0, label: "Private" },
+    { value: 1, label: "Public" },
+];
+
+const CategoryOptions = [
+    { value: 0, label: "Film & Animation" },
+    { value: 1, label: "Autos & Vehicles" },
+    { value: 2, label: "Music" },
+    { value: 3, label: "Pets & Animals" },
+];
+
+function VideoUploadPage() {
+
+    const [VideoTitle, setVideoTitle] = useState("");
+    const [Description, setDescription] = useState("");
+    const [Private, setPrivate] = useState(0);  // Private : 0, Public : 1
+    const [Category, setCategory] = useState("Film & Animation");
+
+    const onTitleChange = (event) => {
+        setVideoTitle(event.currentTarget.value)
+    };
+
+    const onDescriptionChange = (event) => {
+        setDescription(event.currentTarget.value)
+    };
+
+    const onPrivateChange = (event) => {
+        setPrivate(event.currentTarget.value)
+    };
+
+    const onCategoryChange = (event) => {
+        setCategory(event.currentTarget.value)
+    };
+
+    return (
+        <div style={{ maxWidth:'700px', margin:'2rem auto' }}>
+            <div style={{ textAlign:'center', marginBottom:'2rem' }}>
+                <Title level={2}>Upload Video</Title>
+            </div>
+
+            <Form onSubmit>
+                <div style={{ display:'flex', justifyContent:'space-between' }}>
+                    {/* Drop Zone */}
+                    <Dropzone
+                        onDrop
+                        multiple
+                        maxSize>
+                        {({ getRootProps, getInputProps }) => (
+                            <div style={{ width:'300px', height:'240px', border:'1px solid lightgray', display:'flex',
+                                alignItems:'center', justifyContent:'center' }} { ...getRootProps() }>
+                                <input {...getInputProps() } />
+                                <Icon type="plus" style={{ fontSize:'3rem' }} />
+                            </div>
+                        )}
+                    </Dropzone>
+                    {/* Thumbnail */}
+                    <div>
+                        <img src alt />
+                    </div>
+                </div>
+
+                <br/><br/>
+                <label>Title</label>
+                <Input
+                    onChange={onTitleChange}
+                    value={VideoTitle}
+                />
+
+                <br/><br/>
+                <label>Description</label>
+                <TextArea
+                    onChange={onDescriptionChange}
+                    value={Description}
+                />
+
+                <br/><br/>
+                <select onChange={onPrivateChange}>
+                    {PrivateOptions.map((item, index) => (
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}
+                </select>
+
+                <br/><br/>
+                <select onChange={onCategoryChange}>
+                    {CategoryOptions.map((item, index) => (
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}
+                </select>
+
+                <br/><br/>
+                <Button type="primary" size="large" onClick>
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    )
+}
+
+export default VideoUploadPage
