98cfed32340e8febc4952cc60e41c285db8c1ed0
reyeon1209 <reyeon5368@naver.com>
2020-08-29 13:39:35 +0900

    자기자신 구독 못하게 수정

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -22,13 +22,14 @@ function VideoDetailPage(props) {
     }, [])
 
     if (VideoDetails.writer) {
+        const subscribeButton = VideoDetails.writer._id !== localStorage.getItem('userId') && <Subscribe userTo={VideoDetails.writer._id} userFrom={localStorage.getItem('userId')} />
         return (
             <Row gutter={[16, 16]}>
                 <Col lg={18} xs={24} >
                 <div style={{ width: '100%', padding: '3rem 4rem' }}>
                     <video style={{ width: '100%' }} src={`http://localhost:5000/${VideoDetails.filePath}`} controls />
                     <List.Item
-                        actions={[<Subscribe userTo={VideoDetails.writer._id} userFrom={localStorage.getItem('userId')} />]}
+                        actions={[subscribeButton]}
                     >
                         <List.Item.Meta
                             avatar={<Avatar src={VideoDetails.writer.image} />}
